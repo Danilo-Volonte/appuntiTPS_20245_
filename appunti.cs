55e264f74d934bafe16cefaf9d33ca9d88df7622
Danilo-Volonte <167746192+Danilo-Volonte@users.noreply.github.com>
2024-10-07 14:00:09 +0200

    Add files via upload

--- appunti.cs
+++ appunti.cs
@@ -6,33 +6,36 @@ using System.Threading.Tasks;
 using System.Threading;
 using System.Net.NetworkInformation;
 
+int numero0;
+int numero1;
+int numero2;    
+int numero3;
+ // esercizo guarda teams
+
 namespace esercizio11
 {
     internal class Program
     {
         static Thread t1;
         static Thread t2;
+        static Thread t3;
         static void Main(string[] args) //il main è un thread
         {
-                Thread.CurrentThread.Name = "Main"; //stiamo assegnando al thread del Main il nome main
-                
-                t1 = new Thread(saluta);
-                t1.Name = "Thread 1";
-                t2 = new Thread(saluta);
-                t2.Name = "Thread 2";
+            t1 = new Thread(primoCompreso);
+            t1.Name = "Thread 1";
+            t2 = new Thread(secondoCompreso);
+            t2.Name = "Thread 2";
+            t3 = new Thread(terzoCompreso);
+            t3.Name = "Thread 2";
 
-                t1.Start();
-                t2.Start();
-                Console.ReadKey();
-            }
-        private static void saluta() 
-            {
-                Console.WriteLine(Thread.CurrentThread.Name+" sono partito ");
-                Console.WriteLine($"{Thread.CurrentThread.Name}. il mio ID è {Thread.CurrentThread.ManagedThreadId} ");
-                Console.WriteLine(Thread.CurrentThread.Name + " si va a letto!"); //per addormentare il Thread
-                Thread.Sleep(120); //dormo per 120 ms
-                Console.WriteLine(Thread.CurrentThread.Name + " sono tornato ragazzi!");
-            }
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            Console.ReadKey();
+        }
+        private static primoCompreso(string[] args)
+        {
+            numero1=new Random();
         }
     }
-
+}
