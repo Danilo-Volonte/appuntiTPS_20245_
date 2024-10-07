92f955f075e543cfdcabf30ab039984b90d909c3
Danilo-Volonte <167746192+Danilo-Volonte@users.noreply.github.com>
2024-10-07 13:40:55 +0200

    spiegazione
    1) quello che ho detto prima
    2) fare nome per thread e vedere i loro ID

--- appunti.cs
+++ appunti.cs
@@ -10,44 +10,29 @@ namespace esercizio11
 {
     internal class Program
     {
+        static Thread t1;
+        static Thread t2;
         static void Main(string[] args) //il main è un thread
         {
+                Thread.CurrentThread.Name = "Main"; //stiamo assegnando al thread del Main il nome main
+                
+                t1 = new Thread(saluta);
+                t1.Name = "Thread 1";
+                t2 = new Thread(saluta);
+                t2.Name = "Thread 2";
 
-            Thread t1 = new Thread(stampaNumeriDispari); //crea nuovo thread
-            Thread t2 = new Thread(stampaNumeriPari);
-            
-            t1.Start(); //per iniziare un thread
-            t2.Start(); //durante l'sesecuzione del primo thread parte anche il secondo facendo in modo che uno finsca prima dell'obbiettivo 
-            Console.ReadKey(); //fa in modo che il programma non chiuda finché non hai schiacciato un qualsiasi pulsante 
-        }
-
-        private static void stampaNumeriPari()
-        {
-            for (int i = 0; i < 100; i = i + 2)
+                t1.Start();
+                t2.Start();
+                Console.ReadKey();
+            }
+        private static void saluta() 
             {
-                Console.WriteLine("numero " + i);
+                Console.WriteLine(Thread.CurrentThread.Name+" sono partito ");
+                Console.WriteLine($"{Thread.CurrentThread.Name}. il mio ID è {Thread.CurrentThread.ManagedThreadId} ");
+                Console.WriteLine(Thread.CurrentThread.Name + " si va a letto!"); //per addormentare il Thread
+                Thread.Sleep(120); //dormo per 120 ms
+                Console.WriteLine(Thread.CurrentThread.Name + " sono tornato ragazzi!");
             }
         }
-
-        private static void stampaNumeri()
-        {
-            for(int i=1; i<=200;i++) 
-            {
-                Console.WriteLine(i);
-            }
-        } //ottimizzazione delle risorse --> avendom messo tutto dentro una funzione se questa non vine usata allora questa non utilizza spazio inutile 
-
-        private static void stampaNumeriDispari()
-        {
-            for (int i = 1; i < 100; i = i + 2)
-            {
-                Console.WriteLine("numeros " + i);
-            }
-        }
-
-
-
-
-
     }
-}
+
